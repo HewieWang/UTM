0128faf90dd46772ed4bde4c546806eed61353a7
Hannibal <hewiefsociety@gmail.com>
2021-11-15 09:57:11 +0800

    Update utm.user.js

--- utm.user.js
+++ utm.user.js
@@ -9,11 +9,11 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @grant        GM_notification
 // ==/UserScript==
 
 (function() {
     'use strict';
-
     // Your code here...
     function parseBetween(beginString, endString, originalString) {
     var beginIndex = originalString.indexOf(beginString);
@@ -27,6 +27,14 @@
         return null;
     }
     return originalString.substring(substringBeginIndex, substringEndIndex);
+}
+    function doit(title,message,timeout) {
+  GM_notification({
+    title: title,
+    text: message,
+    timeout: timeout,
+    //ondone: ()=>{alert('来自 ondone 事件')}
+  })
 }
     window.onload = function(){
        var url=window.location.host
@@ -44,6 +52,7 @@
                 if (y.document)y=y.document;
                 var count=iframe_src.match(/rowCount%3D(\S*)\//);
                 console.log(count[1])
+                console.log(y.body.querySelector(".ID-start").textContent+"-"+y.body.querySelector(".ID-end").textContent)
                 var ids = y.body.querySelectorAll('._GAwr');
                   [].forEach.call(ids, function(id) {
                       arr1.push(id.querySelector("td span").innerHTML)
@@ -58,17 +67,47 @@
                   });
                   var res=arr1.map((item, idx) => [item, arr2[idx]]);
                   var res2=res.map((item, idx) => [item, arr3[idx]])
-                  console.log(JSON.stringify(res2))
+                  //console.log(JSON.stringify(res2))
                   GM_setValue("google_utm",JSON.stringify(res2));
+                  if(/.*[\u4e00-\u9fa5]+.*$/.test(y.body.querySelector(".ID-start").textContent)) {
+                    var ga_start=y.body.querySelector(".ID-start").textContent.replace("年","-").replace("月","-").replace("日","")
+                    var ga_end=y.body.querySelector(".ID-end").textContent.replace("年","-").replace("月","-").replace("日","")
+                  }else{
+                     ga_start=new Date(y.body.querySelector(".ID-start").textContent).getTime();
+                     ga_end=new Date(y.body.querySelector(".ID-end").textContent).getTime();
+                  }
+                  GM_setValue("ga_start",ga_start);
+                  GM_setValue("ga_end",ga_end);
+                  //doit("GA",y.body.querySelector(".ID-start").textContent+"-"+y.body.querySelector(".ID-end").textContent,3000);
                 //clearInterval(timer1);
               }catch(err){
                  console.log("Iframe Not found")
               }
-          }, 2000);
+          }, 5000);
     }
     if(url.indexOf("facebook")!=-1){
        console.log("FB_UTM")
-       var timer2=setInterval(function(){
+       if(document.getElementsByClassName("_1uz0")[0].textContent.split(":")[1]!=undefined){
+         var fb_time=document.getElementsByClassName("_1uz0")[0].textContent.split(":")[1];
+       }else{
+         fb_time=document.getElementsByClassName("_1uz0")[0].textContent;
+       }
+        console.log(fb_time)
+        fb_time=fb_time.replace("Note","")
+        //console.log(fb_time.substr(0, fb_time.indexOf('–')))
+        //console.log(fb_time.substring(fb_time.indexOf('–') + 1))
+       var fb_start=Date.parse(fb_time.substr(0, fb_time.indexOf('–')).replace(/^\s+|\s+$/g,""));
+       var fb_end=Date.parse(fb_time.substring(fb_time.indexOf('–') + 1).replace(/^\s+|\s+$/g,""));
+       //console.log("GA_time:"+GM_getValue("ga_time"))
+        console.log("FB_time:"+fb_start+"-"+fb_end)
+       if((GM_getValue("ga_start")!=fb_start)||(GM_getValue("ga_end")!=fb_end)){
+         var gg=new Date(GM_getValue("ga_start")).getFullYear() + '-' + new Date(GM_getValue("ga_start")).getMonth() + '-' + new Date(GM_getValue("ga_start")).getDate()
+         var ee=new Date(GM_getValue("ga_end")).getFullYear() + '-' + new Date(GM_getValue("ga_end")).getMonth() + '-' + new Date(GM_getValue("ga_end")).getDate()
+         doit("注意","GA选择时间和FB不符合!请选择正确日期!GA时间是:\n"+gg+"-"+ee);
+       }
+       console.log(fb_start+"-"+fb_end)
+       try {
+         var timer2=setInterval(function(){
            var list = document.getElementsByClassName("_1b33 _4ik4 _4ik5");
            for (var i = 0; i < list.length; i++) {
               if(GM_getValue("google_utm").indexOf(list[i].innerText)!=-1){
@@ -133,6 +172,10 @@
                }
            }
        },3000);
+       } catch (error) {
+         console.error(error);
+         GM_notification("未找到元素，请把Purchase ROAS (Return on Ad Spend)和Website Purchases移到前排", "注意");
+       }
     }
     }
 })();
